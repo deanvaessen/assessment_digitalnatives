65d7ff08df8f3f2d406f9a2c8062c313de321dce
Dean Vaessen <hello@deanvaessen.com>
2016-10-20 00:03:28 +0200

    0.0.3: Form manipulation, basic CSS

--- code/src/components/FormComponent/FormComponent.js
+++ code/src/components/FormComponent/FormComponent.js
@@ -16,145 +16,162 @@ require('./FormComponent.scss');
  */
 
  class ErrorText extends React.Component {
-   render() {
-     return <div style={{ color: '#f00' }}>
-       {this.props.errorText}
-     </div>
-   }
+	 render() {
+		 return <div style={{ color: '#f00' }}>
+			 {this.props.errorText}
+		 </div>
+	 }
  }
 
 class FormComponent extends React.Component {
 	constructor(props) {
 		super(props);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.translateInput = this.translateInput.bind(this);
 	}
 
 	componentWillMount() {
-
 	}
 
 	componentDidMount() {
+		console.log(this.props)
 	}
 
 	componentWillReceiveProps(nextProps) {
-	    // Set default form values (might be appropriate in a different method
-	    this.props.setDefaultValues({
-	      age: 33,
-	      name: 'Sir Fluffalot',
-	    });
-	  }
+			// Set default form values (might be appropriate in a different method
+			this.props.setDefaultValues({
+				arabic: '',
+				english:  '',
+			});
+		}
 
-	  handleSubmit(formStatus, fields) {
-	    if (!formStatus.touched) {
-	      alert('Please fill out the form.');
-	      return;
-	    }
+	handleSubmit(formStatus, fields) {
+		if (!formStatus.touched) {
+			alert('Please fill out the form.');
+			return;
+		}
 
-	    if (!formStatus.valid) {
-	      alert('Please address the errors in the form.');
-	      return;
-	    }
+		if (!formStatus.valid) {
+			alert('Please address the errors in the form.');
+			return;
+		}
 
-	    // All good! Do something with fields.name.value and fields.age.value
-	    console.log(formStatus, fields);
-	  }
+		if (this.props.fields.arabic.value != '' && this.props.fields.english.value != '') {
+			console.log(this.props.fields)
+			alert('You filled in both elements, please clear one.');
+			return;
+		}
+
+		// All good! Do something with fields.name.value and fields.arabic.value
+
+
+		// Initial state
+		let fieldState = this.props.fields;
+
+		// Mutate
+		fieldState.arabic.value = 'test';
+		fieldState.english.value = 'test';
+
+		// Pass back in
+		this.setState(this.props.fields.arabic = {
+			value : fieldState.arabic.value,
+			events : fieldState.arabic.events,
+			valid : fieldState.arabic.valid,
+		});
+
+		this.setState(this.props.fields.english = {
+			value : fieldState.english.value,
+			events : fieldState.english.events,
+			valid : fieldState.english.valid,
+		});
+	}
+
+	translateInput(input, cb){
+
+	}
 
 	render() {
 			const {
-      fields: { age, name },
-      formSubmit,
-    } = this.props;
+			fields: { arabic, english },
+			formSubmit,
+		} = this.props;
 
 		return (
 			<div className="FormComponent__container">
+				<div className="FormComponent__title"><h1>Translation, please</h1></div>
 
-				<div className="FormComponent__title"><p>Hello world</p></div>
-
-				<div className="FormComponent__form">
-					<div className="FormComponent__input">
-						<div className="FormComponent__arabic" >
-
-						</div>
-						<div className="FormComponent__roman" >
-							// input
+				 <form onSubmit={formSubmit(this.handleSubmit)}>
+					<div className="FormComponent__form">
+						<div className="FormComponent__input">
+							<div className="FormComponent__arabic" >
+								<input
+									placeholder="Arabic numeral"
+									type="text"
+									value={arabic.value}
+									{ ...arabic.events }
+								/>
+								<ErrorText { ...arabic.failProps } />
+							</div>
+							<div className="FormComponent__english" >
+								<input
+									placeholder="English"
+									type="text"
+									value={english.value}
+									{ ...english.events }
+								/>
+								<ErrorText { ...english.failProps } />
+							</div>
 						</div>
 					</div>
-				</div>
-					<div className="FormComponent__buttons">
-						<button className="FormComponent__submit">Submit</button>
-						<button className="FormComponent__clear">Clear</button>
-					</div>
-
-					<div>
-					      <form onSubmit={formSubmit(this.handleSubmit)}>
-					        <div>
-					          <input
-					            placeholder="Name"
-					            type="text"
-					            value={name.value}
-					            { ...name.events }
-					          />
-					          <ErrorText { ...name.failProps } />
-					        </div>
-					        <div>
-					          <input
-					            placeholder="Age"
-					            type="text"
-					            value={age.value}
-					            { ...age.events }
-					          />
-					          <ErrorText { ...age.failProps } />
-					        </div>
-					        <div>
-					          <button type="submit">Submit</button>
-					        </div>
-					      </form>
-					    </div>
+						<div className="FormComponent__buttons">
+							<button className="FormComponent__submit" type="submit">Submit</button>
+							<button className="FormComponent__clear" type="button" onClick={this.props.clearForm}>Clear</button>
+						</div>
+				</form>
 			</div>
 		);
 	}
 }
 
 const formousOptions = {
-  fields: {
-    name: {
-      name: 'name',
-      tests: [
-        {
-          critical: true,
-          failProps: {
-            errorText: 'Name is required.',
-          },
-          test(value) {
-            return value !== '';
-          },
-        }
-      ],
-    },
+	fields: {
+		english: {
+			tests: [
+				{
+					critical: true,
+					failProps: {
+						errorText: 'English should be in letters!',
+					},
+					test(value) {
+						return /^[a-zA-Z-]*$/.test(value);
+					},
+				}
+			],
+		},
 
-    age: {
-      name: 'age',
-      tests: [
-        {
-          critical: true,
-          failProps: {
-            errorText: 'Age should be a number.',
-          },
-          test(value) {
-            return /^\d*$/.test(value);
-          },
-        },
-        {
-          critical: false,
-          failProps: {
-            errorText: 'Are you sure you\'re that old? :o',
-          },
-          test(value) {
-            return +value < 120;
-          },
-        },
-      ],
-    },
-  },
+		arabic: {
+			tests: [
+				{
+					critical: true,
+					failProps: {
+						errorText: 'Arabic should be a number',
+					},
+					test(value) {
+						return /^\d*$/.test(value);
+					},
+				},
+/*				{
+					critical: false,
+					failProps: {
+						errorText: 'Are you sure you\'re that old? :o',
+					},
+					test(value) {
+						return +value < 120;
+					},
+				},*/
+			],
+		},
+	},
 };
 
 FormComponent.defaultProps = {
--- code/src/components/root/app.js
+++ code/src/components/root/app.js
@@ -18,17 +18,19 @@ import FormComponent from './../FormComponent/FormComponent';
 /**
  * Component
  */
-let yeomanImage = require('../../images/yeoman.png');
+
+	// let yeomanImage = require('../../images/yeoman.png');
+	//<img src={yeomanImage} alt="Yeoman Generator" />
 
 class AppComponent extends React.Component {
-  render() {
-    return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <FormComponent />
-      </div>
-    );
-  }
+
+	render() {
+		return (
+			<div className="index">
+				<FormComponent />
+			</div>
+		);
+	}
 }
 
 AppComponent.defaultProps = {
